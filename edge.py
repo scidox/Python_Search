958d6302fdd08ca013281a8fe9b7458e0fdaf4d7
Giorgio Attorresi <attorresi.giorgio@gmail.com>
2021-06-16 10:57:15 +0200

    Add edge chromium search

--- edge.py
+++ edge.py
@@ -1,6 +1,6 @@
 import webbrowser    
 url='https://www.bing.com'
-#insert address edge file location
+#insert address edge file location on your system
 edge="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 webbrowser.register('edge', None,webbrowser.BackgroundBrowser(edge))
 webbrowser.get('edge').open_new_tab(url)

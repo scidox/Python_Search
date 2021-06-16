bb6dfc75efec20174ee7e31ef80aba1b18fb1a2d
Giorgio Attorresi <attorresi.giorgio@gmail.com>
2021-06-16 10:53:38 +0200

    Update random_search
    add print iteration

--- random_search.py
+++ random_search.py
@@ -6,18 +6,19 @@ import time
 import os
 name= input ("What content of the text file do you want to search on web? ")
 file=open(name)
-
 cicli=int(input ("how many lines do you want to search for:"))
 
 for n in range(cicli):
-
+        a=n+1
+        print('iteration',+(a),'of',+(cicli))
         lettura=file.readline()
-        value=random.randint(0,11)
+        value=random.randint(4,13)
         print(lettura)
+        
         webbrowser.open('https://www.bing.com/search?q='+str(lettura))
         time.sleep(value)
         #firefox browser
-        #webbrowser.get("firefox").open('https://www.bing.com/search?q='+str(lettura))
+        #webbrowser.get("firefox").open('https://www.bing.com/search?q='+str(lettura))            
 
 time.sleep(10)
-os.system("killall -9 'chrome'")
+#os.system("killall -9 'chrome'")

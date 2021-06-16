4d98f3a6934b80c32df719e6f19f7a91ff7160e7
Giorgio Attorresi <attorresi.giorgio@gmail.com>
2021-06-16 10:28:47 +0200

    Add files via upload
    Edge browser search

---
+++ edge.py
@@ -0,0 +1,6 @@
+import webbrowser    
+url='https://www.bing.com'
+#insert address edge file location
+edge="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+webbrowser.register('edge', None,webbrowser.BackgroundBrowser(edge))
+webbrowser.get('edge').open_new_tab(url)
